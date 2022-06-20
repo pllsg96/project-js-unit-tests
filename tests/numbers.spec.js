f8c19048e5efcf39b57ab958303ae37c3cb4708c
Pedro Luiz <pedroluizlsg@gmail.com>
2022-06-20 16:25:38 -0300

    Adiciona testes do requisito  2 (numbers)

--- tests/numbers.spec.js
+++ tests/numbers.spec.js
@@ -1,6 +1,7 @@
 /* eslint-disable max-len */
 /* eslint-disable no-unused-vars */
 
+const { TestScheduler } = require('jest');
 const numbers = require('../src/numbers');
 
 /*
@@ -16,10 +17,14 @@ const numbers = require('../src/numbers');
 
 describe('2 - Implemente os casos de teste para a função `numbers`', () => {
   it('Verifica se a função `numbers` retorna `true` quando o array contém apenas números e falso caso contrário', () => {
-    fail('Teste vazio!');
+    // fail('Teste vazio!');
     // Escreva um teste em que a função recebe [1, 2, 3, 4, 5] e retorna true
+    expect(numbers([1, 2, 3, 4, 5])).toBe(true);
     // Escreva um teste em que a função recebe [1, 2, '3', 4, 5] e retorna false
+    expect(numbers([1, 2, '3', 4, 5])).toBe(false);
     // Escreva um teste em que a função recebe [1, 'a', 3] e retorna false
+    expect(numbers([1, 'a', 3])).toBe(false);
     // Escreva um teste em que a função recebe [' '] e retorna false
+    expect(numbers([' '])).toBe(false);
   });
 });
